c4ab945e2e2060fc2a7c06632420735ff6aae15d
moreant <moreant@foxmail.com>
2020-06-14 03:22:44 +0800

    feat(info): 作业提醒前端部分

--- miniprogram/pages/info/info.js
+++ miniprogram/pages/info/info.js
@@ -1,18 +1,29 @@
 const db = wx.cloud.database()
 const works = db.collection('works')
-import { formatDay } from "../../utils/myMoment";
+
+import { getTimestamp, formatDay, formatTime, calDays } from "../../utils/myMoment";
 
 Page({
 
   data: {
     loading: true,
-    show: false,
-    actions: [
+    view: {
+      menu: false,
+      sub: false,
+      cal: false,
+      time: false
+    },
+    menu: [
       { name: '修改', color: '#07c160' },
       { name: '删除', color: '#fa5151' },
-      { name: '分享' },
-      { name: '订阅' }
+      { name: '告诉同学', openType: 'share' },
+      { name: '设置提醒' }
     ],
+    sub: {
+      date: formatDay(+ new Date()),
+      time: formatTime(+ new Date()),
+      calDays: ''
+    },
     work: {
       title: '',
       course: '',
@@ -22,10 +33,18 @@ Page({
   },
 
   /**
-   * 显示/关闭 更多菜单
+   * 显示/关闭组件
+   * @param {Object} e 来自 wxml 的事件
+   * @param {String} k 来自 js 的 key
    */
-  show() {
-    this.setData({ show: !this.data.show })
+  showView(e, k) {
+    let key
+    if (k) {
+      key = k
+    } else {
+      key = e.currentTarget.dataset.view
+    }
+    this.setData({ ['view.' + key]: !this.data.view[key] })
   },
 
   /**
@@ -33,21 +52,94 @@ Page({
    */
   onSelect(event) {
     const { name } = event.detail
+    const menus = this.data.menu.map(item => item.name)
     switch (name) {
-      case '修改':
+      // 更新
+      case menus[0]:
         this.update()
         break;
-      case '删除':
+      // 删除
+      case menus[1]:
         this.delect()
         break;
-      case '分享':
+      // 分享
+      case menus[2]:
         console.log("分享")
         break;
-      case '订阅':
-        console.log("订阅")
+      // 订阅
+      case menus[3]:
+        if (this.data.sub.calDays) {
+          this.setData({
+            sub: {
+              date: formatDay(+ new Date()),
+              time: formatTime(+ new Date())
+            }
+          })
+        }
+        this.showView('', 'sub')
         break;
       default:
         break;
+    }
+  },
+
+  /**
+   * 设置日期
+   */
+  setSubDate(e) {
+    console.log(e);
+    this.showView('', 'cal')
+    this.setData({
+      'sub.date': formatDay((e.detail))
+    })
+  },
+
+  /**
+   * 设置时间
+   */
+  setSubTime(e) {
+    console.log(e);
+    this.setData({
+      'sub.time': e.detail.value
+    })
+  },
+
+  /**
+   * 提交订阅
+   */
+  async confirmSub() {
+    const { work: { title, content, end }, sub: { date, time } } = this.data
+    const tempid = 'nLLL7b1sTw4JPR1Znjp281dmd-D6e4l1s-06zXFmRmM'
+    const res = await wx.requestSubscribeMessage({
+      tmplIds: [tempid],
+    })
+    if (res[tempid] != "accept") {
+      // wx.showModal({
+      //   showCancel: false,
+      //   title: '您没有允许设置提醒'
+      // })
+      this.setData({
+        error: '您没有允许设置提醒'
+      })
+    } else {
+      this.showView('', 'sub')
+      console.log(date + ' ' + time);
+      this.setData({
+        'sub.calDays': calDays(date + ' ' + time)
+      })
+
+      // 提交订阅消息
+      const subInfo = {
+        tmplIds,
+        timestamp: getTimestamp(date + ' ' + time),
+        sended: false,
+        title,
+        content,
+        end,
+      }
+
+      // TODO: 完成 获得订阅权限，添加订阅消息到数据库
+      console.log(subInfo);
     }
   },
 
--- miniprogram/utils/myMoment.js
+++ miniprogram/utils/myMoment.js
@@ -1,35 +1,157 @@
-import moment from 'moment'
+import Moment from 'moment'
 
 /**
- * 通过时间戳计算当前距离时间戳的天数
- * @param {Number} Timestamp 时间戳
- * @returns {string} 天数
+ * 计算当前距离时间戳的天数
+ * @param {*} moment 时刻
+ * @returns 
  */
-export function calDays(Timestamp) {
-  const day = moment(Timestamp).calendar(null, {
-    sameDay: '[今天]',
-    nextDay: '[明天]',
-    nextWeek: '[]',
-    lastDay: '[昨天]',
-    lastWeek: '[]',
-    sameElse: '[]'
-  })
-  if (!!day) {
-    return day
-  } else {
-    const days = moment(Timestamp).diff(new Date().valueOf(), "days")
-    if (days > 0) {
-      return `${days + 1} 天后`
-    } else {
-      return `${-days + 1} 天前`
-    }
+export function calDays(moment) {
+  let diff = Moment().diff(Moment(moment), 'days')
+  if (Moment(moment).isBefore() && diff >= 1) {
+    return diff + " 天前"
   }
+  return Moment(moment).calendar(null, {
+    sameDay: '[今天] HH:mm',
+    nextDay: '[明天] HH:mm',
+    nextWeek: 'M[月]D[日]ddd',
+    lastDay: '[]',
+    lastWeek: '[]',
+    sameElse: 'M[月]D[日]ddd'
+  });
+}
+
+/**
+ * 将任意符合条件的时刻转化为 unix 毫秒时间戳
+ * @param {*} any 需要转换的时刻
+ */
+export function getTimestamp(any) {
+  return +Moment(any)
 }
 
 /**
  * 返回 YYYY-MM-DD 格式的日期
- * @param {Number} Timestamp 时间戳
+ * @param {*} moment 时刻
  */
-export function formatDay(Timestamp) {
-  return moment(Timestamp).format('YYYY-MM-DD')
+export function formatDay(moment) {
+  return Moment(moment).format('YYYY-MM-DD')
 }
+
+/**
+ * 返回 HH:mm 格式的时间，如 09:04
+ * @param {*} moment 时刻
+ */
+export function formatTime(moment) {
+  return Moment(moment).format('HH:mm')
+}
+
+// 小程序使用 locale 设置语言有问题，因此手动设置
+Moment.defineLocale('zh-cn', {
+  months: '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split(
+    '_'
+  ),
+  monthsShort: '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split(
+    '_'
+  ),
+  weekdays: '星期日_星期一_星期二_星期三_星期四_星期五_星期六'.split('_'),
+  weekdaysShort: '周日_周一_周二_周三_周四_周五_周六'.split('_'),
+  weekdaysMin: '日_一_二_三_四_五_六'.split('_'),
+  longDateFormat: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'YYYY/MM/DD',
+    LL: 'YYYY年M月D日',
+    LLL: 'YYYY年M月D日Ah点mm分',
+    LLLL: 'YYYY年M月D日ddddAh点mm分',
+    l: 'YYYY/M/D',
+    ll: 'YYYY年M月D日',
+    lll: 'YYYY年M月D日 HH:mm',
+    llll: 'YYYY年M月D日dddd HH:mm',
+  },
+  meridiemParse: /凌晨|早上|上午|中午|下午|晚上/,
+  meridiemHour: function (hour, meridiem) {
+    if (hour === 12) {
+      hour = 0;
+    }
+    if (meridiem === '凌晨' || meridiem === '早上' || meridiem === '上午') {
+      return hour;
+    } else if (meridiem === '下午' || meridiem === '晚上') {
+      return hour + 12;
+    } else {
+      // '中午'
+      return hour >= 11 ? hour : hour + 12;
+    }
+  },
+  meridiem: function (hour, minute, isLower) {
+    var hm = hour * 100 + minute;
+    if (hm < 600) {
+      return '凌晨';
+    } else if (hm < 900) {
+      return '早上';
+    } else if (hm < 1130) {
+      return '上午';
+    } else if (hm < 1230) {
+      return '中午';
+    } else if (hm < 1800) {
+      return '下午';
+    } else {
+      return '晚上';
+    }
+  },
+  calendar: {
+    sameDay: '[今天]LT',
+    nextDay: '[明天]LT',
+    nextWeek: function (now) {
+      if (now.week() !== this.week()) {
+        return '[下]dddLT';
+      } else {
+        return '[本]dddLT';
+      }
+    },
+    lastDay: '[昨天]LT',
+    lastWeek: function (now) {
+      if (this.week() !== now.week()) {
+        return '[上]dddLT';
+      } else {
+        return '[本]dddLT';
+      }
+    },
+    sameElse: 'L',
+  },
+  dayOfMonthOrdinalParse: /\d{1,2}(日|月|周)/,
+  ordinal: function (number, period) {
+    switch (period) {
+      case 'd':
+      case 'D':
+      case 'DDD':
+        return number + '日';
+      case 'M':
+        return number + '月';
+      case 'w':
+      case 'W':
+        return number + '周';
+      default:
+        return number;
+    }
+  },
+  relativeTime: {
+    future: '%s后',
+    past: '%s前',
+    s: '几秒',
+    ss: '%d 秒',
+    m: '1 分钟',
+    mm: '%d 分钟',
+    h: '1 小时',
+    hh: '%d 小时',
+    d: '1 天',
+    dd: '%d 天',
+    M: '1 个月',
+    MM: '%d 个月',
+    y: '1 年',
+    yy: '%d 年',
+  },
+  week: {
+    // GB/T 7408-1994《数据元和交换格式·信息交换·日期和时间表示法》与ISO 8601:1988等效
+    dow: 1, // Monday is the first day of the week.
+    doy: 4, // The week that contains Jan 4th is the first week of the year.
+  },
+});
